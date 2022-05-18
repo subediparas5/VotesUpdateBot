dd0b1cde3fe684297096f296db42d3d9047b42de
Paras <subediparas5@gmail.com>
2022-05-18 19:17:40 +0545

    percentage

--- server.js
+++ server.js
@@ -6,81 +6,96 @@ const rwClient = require('./twitter_client');
 
 const app = express()
 const url = 'https://localelection.ekantipur.com/pradesh-3/district-kathmandu/kathmandu?lng=eng';
-var balen = '';
 const PORT = 8000;
 
-var ii = 0
-var minutes = 1, the_interval = minutes * 60 * 1000;
+let ii = 0
+var minutes = 0.1, the_interval = minutes * 60 * 1000;
+let highest_mayor_votes = ''
 setInterval(async function () {
     axios(url)
         .then(
             async response => {
-                console.log("I am doing my 5 minutes check " + ii++);
+                console.log(`I am doing my ${minutes} minutes check ` + ii++);
                 var html = await response.data;
-                var $ = cheerio.load(html);
-                var data = [];
+                var mayor_substring = await html.substring(
+                    html.indexOf("Mayor"), html.lastIndexOf("Deputy Mayor")
+                )
+                var $ = cheerio.load(mayor_substring);
+                var mayor_data = [];
+                let total_mayor_votes = 0
 
-                $('.candidate-meta-wrapper', html).each(async function () {
+
+                $('.candidate-meta-wrapper', mayor_substring).each(async function () {
                     var candidate_name = $(this).find('.candidate-meta').find('.candidate-name').text()
                     var candidate_party = $(this).find('.candidate-meta').find('.candidate-party-name').text()
                     var votes = $(this).find('.vote-numbers').text()
-                    data.push({
+                    total_mayor_votes += Number(votes)
+                    mayor_data.push({
                         candidate_name,
                         candidate_party,
                         votes,
                     })
                 })
+                //console.log(total_mayor_votes);
 
-                var mayor = [
-                    'Balendra Shah',
-                    'Keshav Sthapit',
-                    'Shirjana Shrestha',
-                    'Madan Das Shrestha',
-                ]
+                var deputy_substring = html.substring(
+                    html.indexOf("Deputy Mayor"), html.lastIndexOf("Balendra Shah")
+                )
+                var $ = cheerio.load(deputy_substring);
+                var deputy_data = [];
+                let total_deputy_votes = 0
 
-                var deputy = [
-                    'Sunita Dangol',
-                    'Rameshwore Shrestha',
-                    'Binita Magaiya',
-                    'Kriti Kansakar',
-                ]
-                var setup = data.filter(obj => {
-                    return obj.candidate_name === mayor[0]
+                $('.candidate-meta-wrapper', deputy_substring).each(async function () {
+                    var candidate_name = $(this).find('.candidate-meta').find('.candidate-name').text()
+                    var candidate_party = $(this).find('.candidate-meta').find('.candidate-party-name').text()
+                    var votes = $(this).find('.vote-numbers').text()
+                    total_deputy_votes += Number(votes)
+                    deputy_data.push({
+                        candidate_name,
+                        candidate_party,
+                        votes,
+                    })
                 })
+                mayor_data.sort(function (b, a) {
+                    return a.votes - b.votes
+                })
+                deputy_data.sort(function (b, a) {
+                    return a.votes - b.votes
+                })
+                //highest votes data to check for data change
+                if (highest_mayor_votes != mayor_data[0].votes) {
+                    highest_mayor_votes = mayor_data[0].votes
 
-                var result = data.filter(obj => {
-                    return obj.candidate_name === mayor[0] || obj.candidate_name === mayor[1] || obj.candidate_name === mayor[2] || obj.candidate_name === mayor[3]
-                })
-                // console.log(result);
-                var deputy_result = data.filter(obj => {
-                    return obj.candidate_name === deputy[0] || obj.candidate_name === deputy[1] || obj.candidate_name === deputy[2] || obj.candidate_name === deputy[3]
-                })
-                console.log(balen,result[0].votes);
-                if (balen != setup[0].votes) {
-                    balen = setup[0].votes
-                    var tweetText = ''
-                    var mayorTweetText = ''
-                    var deputyTweetText = ''
-                    for (i = 0; i <= 3; i++) {
-                        mayorTweetText += result[i].candidate_name + ':' + result[i].votes + '\n'
+                    //top 3 mayor data
+                    let mayor_text = ''
+                    let mayor_loop_total = 0
+                    for (i = 0; i < 3; i++) {
+                        mayor_text += `${mayor_data[i].candidate_name}:${Number(mayor_data[i].votes)}(${(Number(mayor_data[i].votes / total_mayor_votes) * 100).toFixed(2)}%)\n`
+                        mayor_loop_total += Number(mayor_data[i].votes)
                     }
-                    for (i = 0; i <= 3; i++) {
-                        deputyTweetText += deputy_result[i].candidate_name + ':' + deputy_result[i].votes + '\n'
+                    mayor_text += `Others:${total_mayor_votes - mayor_loop_total}:(${(Number(total_mayor_votes - mayor_loop_total) / Number(total_mayor_votes) * 100).toFixed(2)}%)`
+
+
+                    let deputy_text = ''
+                    let deputy_loop_total = 0
+                    //top 2 deputy mayor data
+                    for (i = 0; i < 2; i++) {
+                        deputy_text += `${deputy_data[i].candidate_name}:${Number(deputy_data[i].votes)}(${(Number(deputy_data[i].votes / total_deputy_votes) * 100).toFixed(2)}%)\n`
+                        deputy_loop_total += Number(deputy_data[i].votes)
                     }
-                    tweetText = 'KTM Metro:\nMayor:\n' + mayorTweetText + '\nDeputy:\n' + deputyTweetText + '\nBot Generated!\nSource:ekantipur\n#LocalElections2022\n#LocalElections2079'
-                    var tweet = async () => {
+                    deputy_text += `Others:${total_deputy_votes - deputy_loop_total}:(${(Number(total_deputy_votes - deputy_loop_total) / Number(total_deputy_votes) * 100).toFixed(2)}%)`
+
+                    let tweetText = `KTM Metro\n\nMayor:\n${mayor_text}\n\nDeputy:\n${deputy_text}\n\nBot Generated!\n#LocalElections2022\n#LocalElections2079`
+                    console.log(tweetText);
+                    let tweet = async () => {
                         try {
                             await rwClient.v1.tweet(tweetText)
                         } catch (err) {
                             console.log(err);
                         }
                     }
-                    console.log(tweetText);
                     tweet()
                 }
-
-
-
             }
         )
         .catch(err => {
