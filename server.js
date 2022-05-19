1b5fc4076cfd4ab1d55e55df1c84b1756c122a73
Paras <subediparas5@gmail.com>
2022-05-19 10:18:17 +0545

    commas and fixes

--- server.js
+++ server.js
@@ -38,30 +38,34 @@ setInterval(async function () {
                 })
                 //console.log(total_mayor_votes);
 
-                var deputy_substring = html.substring(
-                    html.indexOf("Deputy Mayor"), html.lastIndexOf("Balendra Shah")
-                )
-                var $ = cheerio.load(deputy_substring);
-                var deputy_data = [];
-                let total_deputy_votes = 0
+                // var deputy_substring = html.substring(
+                //     html.indexOf("Deputy Mayor"), html.lastIndexOf("Balendra Shah")
+                // )
+                // var $ = cheerio.load(deputy_substring);
+                // var deputy_data = [];
+                // let total_deputy_votes = 0
 
-                $('.candidate-meta-wrapper', deputy_substring).each(async function () {
-                    var candidate_name = $(this).find('.candidate-meta').find('.candidate-name').text()
-                    var candidate_party = $(this).find('.candidate-meta').find('.candidate-party-name').text()
-                    var votes = $(this).find('.vote-numbers').text()
-                    total_deputy_votes += Number(votes)
-                    deputy_data.push({
-                        candidate_name,
-                        candidate_party,
-                        votes,
-                    })
-                })
+                // $('.candidate-meta-wrapper', deputy_substring).each(async function () {
+                //     var candidate_name = $(this).find('.candidate-meta').find('.candidate-name').text()
+                //     var candidate_party = $(this).find('.candidate-meta').find('.candidate-party-name').text()
+                //     var votes = $(this).find('.vote-numbers').text()
+                //     total_deputy_votes += Number(votes)
+                //     deputy_data.push({
+                //         candidate_name,
+                //         candidate_party,
+                //         votes,
+                //     })
+                // })
                 mayor_data.sort(function (b, a) {
                     return a.votes - b.votes
                 })
-                deputy_data.sort(function (b, a) {
-                    return a.votes - b.votes
-                })
+
+                function numberWithCommas(x) {
+                    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                }
+                // deputy_data.sort(function (b, a) {
+                //     return a.votes - b.votes
+                // })
                 //highest votes data to check for data change
                 if (highest_mayor_votes != mayor_data[0].votes) {
                     highest_mayor_votes = mayor_data[0].votes
@@ -70,22 +74,21 @@ setInterval(async function () {
                     let mayor_text = ''
                     let mayor_loop_total = 0
                     for (i = 0; i < 3; i++) {
-                        mayor_text += `${mayor_data[i].candidate_name}:${Number(mayor_data[i].votes)}(${(Number(mayor_data[i].votes / total_mayor_votes) * 100).toFixed(2)}%)\n`
+                        mayor_text += `${mayor_data[i].candidate_name}: ${numberWithCommas(Number(mayor_data[i].votes))} (${(Number(mayor_data[i].votes / total_mayor_votes) * 100).toFixed(2)}%)\n`
                         mayor_loop_total += Number(mayor_data[i].votes)
                     }
-                    mayor_text += `Others:${total_mayor_votes - mayor_loop_total}:(${(Number(total_mayor_votes - mayor_loop_total) / Number(total_mayor_votes) * 100).toFixed(2)}%)`
+                    mayor_text += `Others:${numberWithCommas(total_mayor_votes - mayor_loop_total)}:(${(Number(total_mayor_votes - mayor_loop_total) / Number(total_mayor_votes) * 100).toFixed(2)}%)`
 
 
-                    let deputy_text = ''
-                    let deputy_loop_total = 0
-                    //top 2 deputy mayor data
-                    for (i = 0; i < 2; i++) {
-                        deputy_text += `${deputy_data[i].candidate_name}:${Number(deputy_data[i].votes)}(${(Number(deputy_data[i].votes / total_deputy_votes) * 100).toFixed(2)}%)\n`
-                        deputy_loop_total += Number(deputy_data[i].votes)
-                    }
-                    deputy_text += `Others:${total_deputy_votes - deputy_loop_total}:(${(Number(total_deputy_votes - deputy_loop_total) / Number(total_deputy_votes) * 100).toFixed(2)}%)`
-
-                    let tweetText = `KTM Metro\n\nMayor:\n${mayor_text}\n\nDeputy:\n${deputy_text}\n\nBot Generated!\n#LocalElections2022\n#LocalElections2079`
+                    // let deputy_text = ''
+                    // let deputy_loop_total = 0
+                    // //top 2 deputy mayor data
+                    // for (i = 0; i < 2; i++) {
+                    //     deputy_text += `${deputy_data[i].candidate_name}:${Number(deputy_data[i].votes)}(${(Number(deputy_data[i].votes / total_deputy_votes) * 100).toFixed(2)}%)\n`
+                    //     deputy_loop_total += Number(deputy_data[i].votes)
+                    // }
+                    // deputy_text += `Others:${total_deputy_votes - deputy_loop_total}:(${(Number(total_deputy_votes - deputy_loop_total) / Number(total_deputy_votes) * 100).toFixed(2)}%)`
+                    let tweetText = `KTM Metro Mayor:\n${'-'.repeat([30])}\n${mayor_text}\n${'-'.repeat([30])}\nTotal Votes Counted:${numberWithCommas(total_mayor_votes)}\n\nBot Generated!\n#LocalElections2022\n#LocalElections2079`
                     console.log(tweetText);
                     let tweet = async () => {
                         try {
